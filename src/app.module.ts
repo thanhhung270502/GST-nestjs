c35c6a0f5a40028b8fcbac3e3a7aa3c9fb54571d
thanhhung270502 <thanhhung270502@gmail.com>
2023-04-25 16:30:42 +0700

    Connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ClimatesModule } from './climates/climates.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ClimatesModule],
+  imports: [
+    ClimatesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Petykute161',
+      database: 'climate-management',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
